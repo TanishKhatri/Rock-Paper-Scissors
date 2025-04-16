c02cbc78c86f0dba8c6d5f24b5cf00ac9a511dd4
Tanish Khatri <tanishkhatriofficial@gmail.com>
2025-04-16 10:55:10 +0000

    Completed the UI of rps

--- rock-paper.js
+++ rock-paper.js
@@ -1,9 +1,16 @@
 
 let humanScore = 0;
 let computerScore = 0;
+let humanScoreDiv = document.querySelector("#playerScore");
+let computerScoreDiv = document.querySelector("#computerScore");
+let winner = document.querySelector("#winner");
+
 let tie = "Tie! No winner";
 let win = "You won! Congratulations!";
 let lose = "You Lost!";
+
+let wonFlag = 0;
+
 
 function randomRangeThree () {
   if((0 <= Math.random()) && (Math.random() < 0.33))
@@ -30,57 +37,62 @@ function getComputerChoice (choice) {
   }
 }
 
-function getHumanChoice () {
-  userChoice = prompt("Please type your choice: \n 1. Rock \n 2. Paper \n 3. Scissors");
-  userChoice = userChoice.toUpperCase();
+// function getHumanChoice () {
+//   userChoice = userChoice.toUpperCase();
 
-  if ((userChoice !== "ROCK") && (userChoice !== "PAPER") && (userChoice !== "SCISSORS")) {
-    console.log("Invalid choice: Please reload the page to try again");
-    return 1;
+//   if ((userChoice !== "ROCK") && (userChoice !== "PAPER") && (userChoice !== "SCISSORS")) {
+//     console.log("Invalid choice: Please reload the page to try again");
+//     return 1;
+//   }
+
+//   console.log(userChoice);
+//   return userChoice;
+// }
+function addHumanScore () {
+  humanScore += 1;
+  humanScoreDiv.textContent = `Player Score: ${humanScore}`;
+  if (humanScore === 5 && wonFlag === 0) {
+    winner.textContent = win;
+    wonFlag = 1;
   }
+}
 
-  console.log(userChoice);
-  return userChoice;
+function addComputerScore () {
+  computerScore += 1;
+  computerScoreDiv.textContent = `Computer Score: ${computerScore}`;
+  if (computerScore === 5 && wonFlag === 0) {
+    winner.textContent = lose;
+    wonFlag = 2;
+  }
 }
 
 function playRound (humanChoice, computerChoice) {
   if (humanChoice === "ROCK") {
-    if (computerChoice === "ROCK") {
-      console.log(tie);
-    } else if (computerChoice === "SCISSORS") {
-      console.log(win);
-      humanScore += 1;      
+    if (computerChoice === "SCISSORS") {
+      addHumanScore();     
     } else {
-      console.log(lose);
-      computerScore += 1;
+      addComputerScore();
     }
   } else if (humanChoice === "SCISSORS") {
-    if (computerChoice === "SCISSORS") {
-      console.log(tie);
-    } else if (computerChoice === "PAPER") {
-      console.log(win);
-      humanScore += 1;      
+     if (computerChoice === "PAPER") {
+      addHumanScore();     
     } else {
-      console.log(lose);
-      computerScore += 1;
+      addComputerScore();
     }
   } else {
-    if (computerChoice === "PAPER") {
-      console.log(tie);
-    } else if (computerChoice === "ROCK") {
-      console.log(win);
-      humanScore += 1;      
+     if (computerChoice === "ROCK") {
+      addHumanScore();      
     } else {
-      console.log(lose);
-      computerScore += 1;
+      addComputerScore();
     }
   }
 }
 
-function playGame () {
-  for(let i=0; i < 5; i++){
-    playRound(getHumanChoice() , getComputerChoice(randomRangeThree()));
-  }
-}
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
 
-playGame();
+rockButton.addEventListener("click", function(){playRound("ROCK", getComputerChoice(randomRangeThree()))});
+paperButton.addEventListener("click", function(){playRound("PAPER", getComputerChoice(randomRangeThree()))});
+scissorsButton.addEventListener("click", function(){playRound("SCISSORS", getComputerChoice(randomRangeThree()))});
+
